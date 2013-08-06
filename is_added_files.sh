a73c57161066285a9b03fc75180788802050a934
liar666 <gm@presans.com>
2013-08-06 16:07:50 +0200

    Lastest config files... Network if-up.d still does not configures proxy correctly

--- is_added_files.sh
+++ is_added_files.sh
@@ -1,1 +1,3 @@
+# TO BE RUN IN ~/
 for i in Softs/* ; do echo $i ;  diff "$i" "Work/PreSans/Config/ConfigFiles/Linux/$i" ; done
+for i in `find  -maxdepth 1 ~/Work/PreSans/Config/ConfigFiles/Linux/ -name '.*'` ; do echo "***** $i" ; diff $i `basename $i` ; done
